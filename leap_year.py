f2d496c5cf80f022d28fc7b1bfe58566d01af43c
Lucadagostino0907 <ldagostino2@mail.austral.edu.ar>
2025-09-01 12:16:08 -0300

    Add files via upload

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,14 @@
 def leap_year():
-    print("TO DO")
+   
+
+    año_i=int(input("ingrese un año: "))
+    año_f= float(año_i)
+    año_div = año_f%4
+    año_cent = año_f%400
+    if año_div == 0 and año_cent == 0:
+        print(f"El año {año_i} es bisiesto")
+
+    else:
+
+        print(f"El año {año_i} no es bisiesto")
+
--- line.py
+++ line.py
@@ -1,2 +1,37 @@
 def line():
-    print("TO DO")
+    
+
+    coe_A = float(input("Ingrese el coeficiente A: "))
+    coe_B = float(input("Ingrese el coeficiente B: "))
+    coe_x1 =float(input("Ingrese el coeficiente X1: "))
+    coe_x2 = float(input("Ingrese el coeficinte X2: "))
+
+    print(f"El coeficiente A de su ecuación de la recta es: {coe_A}")
+    print(f"El coeficiente B de su ecuación de la recta es: {coe_B}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {coe_x1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {coe_x2}")
+
+
+    print("\nPara la siguiente ecuación:")
+
+    print(f"\tY = {coe_A}X + {coe_B}") 
+
+
+    
+    Y1= (coe_A*coe_x1) + coe_B
+    Y2= (coe_A*coe_x2) + coe_B
+    
+    P1= (coe_x1 , Y1)
+    P2= (coe_x2 , Y2)
+
+    print("\nDados los siguientes puntos:")
+    print(f"\tP1 {P1}")
+    print(f"\tP2 {P2}")
+
+    X_dis = coe_x2 - coe_x1
+    Y_dis = Y2 - Y1
+
+    distancia = (X_dis**2 + Y_dis**2)**0.5
+
+    print(f"\nLa distancia entre ellos es: {distancia}")
+
